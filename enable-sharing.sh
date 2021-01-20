5a985c393816da320956f4ae77998f8b7e0fe894
rontogiannis <68854858+rontogiannis@users.noreply.github.com>
2021-01-20 19:29:15 +0200

    Update enable-sharing.sh

--- enable-sharing.sh
+++ enable-sharing.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 mkdir /home/$1/shared
 sudo mount -t vboxsf shared /home/$1/shared
-echo '' >> /etc/fstab
-echo -e 'shared\t/home/'$1'/shared\tvboxsf\tdefaults\t0\t0' >> /etc/fstab
-echo '' >> /etc/modules
-echo 'vboxsf' >> /etc/modules
+echo -e 'shared\t/home/'$1'/shared\tvboxsf\tdefaults\t0\t0' | sudo tee -a /etc/fstab
+echo 'vboxsf' | sudo tee -a /etc/modules
