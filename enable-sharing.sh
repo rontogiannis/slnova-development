70a10422213d3bce30f7343e26b5481348577438
rontogiannis <68854858+rontogiannis@users.noreply.github.com>
2021-01-20 16:40:09 +0200

    Add files via upload

--- enable-sharing.sh
+++ enable-sharing.sh
@@ -2,6 +2,6 @@
 mkdir ~/shared
 sudo mount -t vboxsf shared ~/shared
 echo '' >> /etc/fstab
-echo 'shared\t~/shared\tvboxsf\tdefaults\t0\t0' >> /etc/fstab
+echo -e 'shared\t~/shared\tvboxsf\tdefaults\t0\t0' >> /etc/fstab
 echo '' >> /etc/modules
 echo 'vboxsf' >> /etc/modules
